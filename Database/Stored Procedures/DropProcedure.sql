e87aed77891d170ed4cee6e6a491664499c2617c
Kendry Arrazcaeta <89476205+kendry191@users.noreply.github.com>
2023-05-04 22:40:39 -0500

    Update DropProcedure.sql

--- Database/Stored Procedures/DropProcedure.sql
+++ Database/Stored Procedures/DropProcedure.sql
@@ -1,18 +1,18 @@
 USE ApplicationDB;
 
 --The following are discontinued tables
-DROP TABLE IF EXISTS DiscountDeterminant;
-DROP TABLE IF EXISTS Discount;
-DROP TABLE IF EXISTS CategoryProduct;
-DROP TABLE IF EXISTS Category;
-DROP TABLE IF EXISTS Stock;
+DROP TABLE DiscountDeterminant;
+DROP TABLE Discount;
+DROP TABLE CategoryProduct;
+DROP TABLE Category;
+DROP TABLE Stock;
 
 --The following are current tables
-DROP TABLE IF EXISTS SaleProduct;
-DROP TABLE IF EXISTS Sale;
-DROP TABLE IF EXISTS CartProduct;
-DROP TABLE IF EXISTS Cart;
-DROP TABLE IF EXISTS Product;
-DROP TABLE IF EXISTS PaymentMethod;
-DROP TABLE IF EXISTS Address;
-DROP TABLE IF EXISTS Account;
+DROP TABLE SaleProduct;
+DROP TABLE Sale;
+DROP TABLE CartProduct;
+DROP TABLE Cart;
+DROP TABLE Product;
+DROP TABLE PaymentMethod;
+DROP TABLE Address;
+DROP TABLE Account;
